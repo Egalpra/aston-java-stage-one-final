8ccdbe3172a158cf17646f8d198a27aa6276c7a9
Nikita Zworykin <sir.zworikin@yandex.ru>
2025-10-16 17:33:00 +0300

    feature: add entity

---
+++ src/main/java/ru/aston/sort_app/model/Movie.java
@@ -0,0 +1,30 @@
+package ru.aston.sort_app.model;
+
+public class Movie {
+    private int id;
+    private String name;
+    private int year;
+    private String director;
+    private double rate;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+}
+
