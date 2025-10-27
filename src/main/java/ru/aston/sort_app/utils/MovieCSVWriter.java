19099569eb03eeaa44ee900dd59e8577911f708d
Nikita Zworykin <sir.zworikin@yandex.ru>
2025-10-28 00:42:06 +0300

    fix writer

--- src/main/java/ru/aston/sort_app/utils/MovieCSVWriter.java
+++ src/main/java/ru/aston/sort_app/utils/MovieCSVWriter.java
@@ -9,28 +9,28 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class MovieCSVWriter {
     private final Path pathToCSV;
-    private final Logger logger;
+    private boolean isNewFile = false;
 
     public MovieCSVWriter(String path) throws IOException {
         this.pathToCSV = Paths.get(path);
-        this.logger = Logger.getLogger(this.getClass().getName());
-
         createFileIfNotExist(pathToCSV);
     }
 
     private void createFileIfNotExist(Path path) throws IOException {
         if (!Files.exists(path)) {
             Files.createFile(path);
+            isNewFile = true;
         }
     }
 
     private String toCSV(Movie movie) {
         return String.format(
+                Locale.US,
                 "%d,\"%s\",%d,\"%s\",%.1f",
                 movie.getId(),
                 movie.getName().replaceAll("\"", "\"\""),
@@ -41,11 +41,20 @@ public class MovieCSVWriter {
     }
 
     public void writeMovies(List<Movie> movies) {
-        String content = movies.stream()
+        StringBuilder sb = new StringBuilder();
+
+        if (!movies.isEmpty() && !isNewFile) {
+            sb.append("\n");
+        }
+
+        sb.append(
+                movies.stream()
                 .map(this::toCSV)
-                .collect(Collectors.joining(System.lineSeparator()));
+                .collect(Collectors.joining(System.lineSeparator()))
+        );
+
         try {
-            Files.writeString(this.pathToCSV, content, Charset.defaultCharset(), StandardOpenOption.APPEND);
+            Files.writeString(this.pathToCSV, sb.toString(), Charset.defaultCharset(), StandardOpenOption.APPEND);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
