14123ea27a493fff0a1e0d0eb8454e79f6f83665
Nikita Zworykin <sir.zworikin@yandex.ru>
2025-10-28 13:14:26 +0300

    fix writer

--- src/main/java/ru/aston/sort_app/utils/MovieCSVWriter.java
+++ src/main/java/ru/aston/sort_app/utils/MovieCSVWriter.java
@@ -3,6 +3,8 @@ package ru.aston.sort_app.utils;
 import ru.aston.sort_app.model.Movie;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -40,21 +42,59 @@ public class MovieCSVWriter {
         );
     }
 
+    private boolean fileEndsWithNewline(Path path) throws IOException {
+        long size = Files.size(path);
+        if (size == 0) return false;
+
+        try (SeekableByteChannel channel = Files.newByteChannel(path, StandardOpenOption.READ)) {
+            channel.position(size - 1);
+            ByteBuffer buffer = ByteBuffer.allocate(1);
+            channel.read(buffer);
+            buffer.flip();
+            byte lastByte = buffer.get();
+
+            if (lastByte == (byte) '\n') {
+                return true;
+            }
+
+            if (lastByte == (byte) '\r') {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void writeMovies(List<Movie> movies) {
+        if (movies == null || movies.isEmpty()) return;
+
         StringBuilder sb = new StringBuilder();
 
-        if (!movies.isEmpty() && !isNewFile) {
-            sb.append("\n");
-        }
-
-        sb.append(
-                movies.stream()
-                .map(this::toCSV)
-                .collect(Collectors.joining(System.lineSeparator()))
-        );
-
         try {
-            Files.writeString(this.pathToCSV, sb.toString(), Charset.defaultCharset(), StandardOpenOption.APPEND);
+            boolean needLeadingNewline = false;
+
+            if (!isNewFile) {
+                long size = Files.size(pathToCSV);
+                if (size > 0) {
+                    needLeadingNewline = !fileEndsWithNewline(pathToCSV);
+                }
+            }
+
+            if (needLeadingNewline) {
+                sb.append(System.lineSeparator());
+            }
+
+            sb.append(
+                    movies.stream()
+                            .map(this::toCSV)
+                            .collect(Collectors.joining(System.lineSeparator()))
+            );
+
+            Files.writeString(
+                    this.pathToCSV,
+                    sb.toString(),
+                    Charset.defaultCharset(),
+                    StandardOpenOption.APPEND
+            );
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
